53b3fd8416b6108a181d8d1c3143bcbc9358b730
Moishe Keselman <shimoshik@gmail.com>
2020-12-03 21:20:29 -0800

    Note table selection works

---
+++ build/precache-manifest.f3c66a18792c7fef2895ab89ff3ff2fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2ea20572a978958c13908604355d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5b2385e1167b30988b9",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "cdae3e7e7a8a506c9002",
+    "url": "/static/js/2.97540e65.chunk.js"
+  },
+  {
+    "revision": "adeb8c39e02302a4495add7d323af481",
+    "url": "/static/js/2.97540e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b2385e1167b30988b9",
+    "url": "/static/js/main.96b4cd5d.chunk.js"
+  },
+  {
+    "revision": "9395fa53102dbd479c75",
+    "url": "/static/js/runtime-main.83b4d353.js"
+  },
+  {
+    "revision": "7ed4b6a5bca63723fcfc69217f85ddb9",
+    "url": "/static/media/BadScript-Regular.7ed4b6a5.ttf"
+  }
+]);
--- src/assets/components/Orders.js
+++ src/assets/components/Orders.js
@@ -7,8 +7,13 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Title from "./Title";
-import { Switch, FormControlLabel, TableSortLabel, Checkbox, Grid, Button, Typography } from "@material-ui/core";
 import { green, red } from "@material-ui/core/colors";
+
+import { Switch, FormControlLabel, TableSortLabel, Checkbox, Grid, Button, Typography } from "@material-ui/core";
+import withSelections from 'react-item-select';
+// import OrdersTable from "./OrdersTable";
+
+const items = [{id: 1, name: 'One'}, {id: 2, name: 'Two'}, {id: 3, name: 'Three'}];
 
 Object.filter = (obj, predicate) =>
   Object.keys(obj)
@@ -50,20 +55,67 @@ const buttonTheme = createMuiTheme({
   }
 })
 
+
 // const sortObject = (obj) =>
 //   Object.keys(obj)
 //     .sort()
 //     .reduce((res, key) => ((res[key] = obj[key]), res), {});
 
+// input: a function to call when the selected notes change
+// output: a list of currently selected notes, a function to set the notes (setSelectedNotes())
+function useSelected(onChangeCallback){
+  const [selectedNotes, setSelectedNotes] = React.useState([]);
+  
+  function retFunc(noteId){
+    console.log(noteId);
+    setSelectedNotes([...selectedNotes, noteId])
+  }
 
-export default function Orders(props) {
-  const [noteData, setNoteData] = React.useState({});
+  React.useEffect(()=>{
+    onChangeCallback();
+  }, [selectedNotes, retFunc]);
+
+  return [selectedNotes, retFunc];
+}
+
+
+export default withSelections((props) => {
+
+  const {
+    areAllIndeterminate,
+    areAllSelected,
+    areAnySelected,
+    selectedCount,
+    handleClearAll,
+    handleSelect,
+    handleSelectAll,
+    isItemSelected,
+    selections,
+    firebase
+  } = props;
+
+  const segmentStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  };
+
+  // Firebase Data
+  const [noteData, setNoteData] = React.useState([]);
+  
+  // notes component
   const [notesTable, setNotesTable] = React.useState([]);
 
+  // what to sort by
   const [orderCat, setOrderCat] = React.useState("approved");
+
+  // up or down
   const [order, setOrder] = React.useState(false);
+
+  // if the note table needs to be updates
   const [update, setUpdate] = React.useState(true);
 
+  // Selected notes ids and length
   const [selectedNotes, setSelectedNotes] = React.useState([])
   const [selectedNotesNum, setSelectedNotesNum] = React.useState(0)
 
@@ -75,9 +127,14 @@ export default function Orders(props) {
   function gotStatData(data) {
     data = data.val();
     // setNoteData({ ...data, ...noteData });
-    setNoteData(data)
+    var dataArray = []
+    Object.keys(data).forEach(id => {
+      dataArray.push({id: id, ...data[id]});
+    })
+    console.log(dataArray)
+    setNoteData(dataArray)
     
-    setNotesTableWrapper(data)
+    // setNotesTableWrapper(data)
   }
   function errData(err) {
     console.log("Error!");
@@ -85,7 +142,7 @@ export default function Orders(props) {
   }
 
   function getDataRef(approval) {
-    return props.firebase.db
+    return firebase.db
       .ref("formData")
       // .orderByChild(orderCat)
       // .equalTo(approval)
@@ -100,43 +157,6 @@ export default function Orders(props) {
   }
   function getData(ref) {
     ref.on("value", gotStatData, errData);
-  }
-  // const switchApproval = (event) => {
-  //   console.log(event.target.checked);
-  //   setApproved(event.target.checked);
-  //   if (
-  //     Object.keys(
-  //       Object.filter(
-  //         noteData,
-  //         (approve) => event.target.checked == approve.approved
-  //       )
-  //     ).length === 0
-  //   ) {
-  //     getData(getDataRef(event.target.checked));
-  //   }
-  // };
-
-  function onSelected(event, noteId, checked){
-    // const checked = event.target.checked;
-    // console.log(noteId);
-    
-    const idxNote = selectedNotes.indexOf(noteId)
-    if (checked){
-      if (idxNote == -1){
-        selectedNotes.push(noteId);
-      }
-    }
-    else if (idxNote > -1){
-      selectedNotes.splice(idxNote, 1);
-    }
-    setSelectedNotes(selectedNotes);
-    setSelectedNotesNum(selectedNotes.length);
-    // console.log(selectedNotes)
-  }
-  function onUnselectAll(){
-    setSelectedNotes([]);
-    setSelectedNotesNum(0);
-    setNotesTableWrapper(noteData)
   }
 
   function onChangeNoteStatus(stateType, state){
@@ -158,58 +178,144 @@ export default function Orders(props) {
       ? 1
       : -1
   )
+  //stat that stored the checkmarks for each components [t, f, t, t, f]
+  //dictionary: each key would be the note id, each item would be a true/false. maybe only keep the ones that have been changed as well. 
 
-  function setNotesTableWrapper(data, orderCategory=orderCat, orderDir=order){
-    console.log("setNotesTableWrapper ORDER:", order, "CATEGORY:", orderCat)
-    console.log("setNoteTableWrapper DATA:", data)
-    setNotesTable(Object.keys(data)
-      .sort((a, b) => sortNoteData(a,b,orderCategory,orderDir, data))
-      .map((id) => (
-        <Note noteData={data[id]} id={id} onChange={onSelected} key={data, id} isChecked={selectedNotes} update={update}/>
-    )));
-    // console.log(notesTable)
-    setUpdate(false);
-  }
 
   console.log("main method NOTEDATA:" , noteData)
+  console.log("main method NOTETABLE:", notesTable)
+  console.log("main method SELECTEDNOTES", selections)
 
   return (
     <React.Fragment>
       <Title>Recent Notes</Title>
 
-      <ThemeProvider theme={buttonTheme}>
-        <Grid container spacing={2}>
-          <Grid item><Button variant="contained" color="primary" onClick={()=>onChangeNoteStatus("approved", true)}>Approve</Button></Grid>
-          <Grid item><Button variant="contained" color="secondary" onClick={()=>onChangeNoteStatus("approved", false)}>Unapprove</Button></Grid>
-          <Grid item><div/></Grid>
-          <Grid item><Button variant="contained" color="primary" onClick={()=>onChangeNoteStatus("sent", true)}>Send</Button></Grid>
-          <Grid item><Button variant="contained" color="secondary" onClick={()=>onChangeNoteStatus("sent", false)}>Unsend</Button></Grid>
-          <Grid item><div/></Grid>
-          <Grid item><Button variant="contained" color="grey" onClick={()=>onUnselectAll()}>Unselect All</Button></Grid>
+      <div>
+        <div textAlign="left" style={segmentStyle}>
 
-          <Grid item><Typography variant="subtitle1" style={{color:"#555"}}>{selectedNotesNum > 0? selectedNotesNum + " Notes Selected" : ""}</Typography></Grid>
-        </Grid>
-      </ThemeProvider>
+          <ThemeProvider theme={buttonTheme}>
+            <Grid container spacing={2} alignItems="center"> 
+              <Grid item><Button variant="contained" color="primary" onClick={()=>onChangeNoteStatus("approved", true)}>Approve</Button></Grid>
+              <Grid item><Button variant="contained" color="secondary" onClick={()=>onChangeNoteStatus("approved", false)}>Unapprove</Button></Grid>
+              <Grid item><div/></Grid>
+              <Grid item><Button variant="contained" color="primary" onClick={()=>onChangeNoteStatus("sent", true)}>Send</Button></Grid>
+              <Grid item><Button variant="contained" color="secondary" onClick={()=>onChangeNoteStatus("sent", false)}>Unsend</Button></Grid>
+              <Grid item><div/></Grid>
+              {/* <Grid item><Button variant="contained" color="grey" onClick={handleClearAll}>Unselect All</Button></Grid> */}
+              <Grid item><Typography variant="subtitle1" style={{color:"#555"}}>{selectedCount > 0? selectedCount + " Notes Selected" : ""}</Typography></Grid>
+            </Grid>
+          </ThemeProvider>
 
-      {/* <FormControlLabel
-        control={<Switch checked={approved} onChange={switchApproval} />}
-        label={approved ? "Approved" : "Non Approved"}
-      /> */}
+          {/* {!areAnySelected && <span>Select items in the table below</span>}
+          <div style={{ visibility: areAnySelected ? 'visible' : 'hidden' }}>
+            <span style={{marginRight: '8px'}}>{selectedCount} selected</span>
+            <Button basic onClick={handleClearAll}>Clear</Button>
+          </div>
+          <div>
+            <span>{items.length} Members</span>
+          </div> */}
+        </div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Checkbox
+                  checked={areAllSelected(noteData)}
+                  indeterminate={areAllIndeterminate(noteData)}
+                  onChange={() => handleSelectAll(noteData)}
+                />
+              </TableCell>
+              {/* <TableCell>USERNAME</TableCell>
+              <TableCell>FIRST</TableCell>
+              <TableCell>LAST</TableCell> */}
+              <TableCell>Where To</TableCell>
+              {/* <TableCell>Approved</TableCell> */}
+              <TableCell>
+                <TableSortLabel
+                  active={orderCat === "approved"}
+                  direction={order ? "asc" : "desc"}
+                  onClick={() => {
+                    console.log('approved order: '+ !order);
+                    setOrderCat("approved");
+                    setOrder(!order);
+                    // setNotesTableWrapper(noteData, "approved", !order);
+                  }}
+                >
+                  Approved
+                  {
+                    // for debugging purposes
+                    <span className={classes.visuallyHidden}>
+                      {order === "desc"
+                        ? "sorted descending"
+                        : "sorted ascending"}
+                    </span>
+                  }
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={orderCat === "sent"}
+                  direction={order ? "asc" : "desc"}
+                  onClick={() => {
+                    console.log('sent order: '+ !order);
+                    setOrder(!order);
+                    setOrderCat("sent");
+                    // setNotesTableWrapper(noteData, "sent", !order);
+                  }}
+                >
+                  Sent
+                  {
+                    // for debugging purposes
+                    <span className={classes.visuallyHidden}>
+                      {order === "desc"
+                        ? "sorted descending"
+                        : "sorted ascending"}
+                    </span>
+                  }
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>Sender</TableCell>
+              <TableCell align="right">Note</TableCell>
+            </TableRow>
+          </TableHead>
 
-      <Table size="small">
+
+          <TableBody>
+            {noteData.map(note => (
+              <TableRow key={note.id}>
+                <TableCell>
+                  <Checkbox
+                    checked={isItemSelected(note.id)}
+                    onChange={() => handleSelect(note.id)}
+                  />
+                </TableCell>
+                <TableCell>{note.facility}</TableCell>
+                <TableCell>{note.approved ? "✔️" : "❌"}</TableCell>
+                <TableCell>{note.sent ? "✔️" : "❌"}</TableCell>
+                <TableCell>{note.sender}</TableCell>
+                <TableCell align="right">{note.note}</TableCell>
+
+              </TableRow>
+          ))}
+          </TableBody>
+        </Table>
+      </div>
+
+
+      {/* <Table size="small">
         <TableHead>
           <TableRow>
             <TableCell>Select</TableCell>
             <TableCell>Where To</TableCell>
-            {/* <TableCell>Approved</TableCell> */}
+            {/* <TableCell>Approved</TableCell>
             <TableCell>
               <TableSortLabel
                 active={orderCat === "approved"}
                 direction={order ? "asc" : "desc"}
                 onClick={() => {
                   console.log('approved order: '+ !order);
-                  setOrder(!order);
                   setOrderCat("approved");
+                  setOrder(!order);
                   setNotesTableWrapper(noteData, "approved", !order);
                 }}
               >
@@ -255,7 +361,7 @@ export default function Orders(props) {
             .sort((a, b) => sortNoteData(a,b,orderCat,order))
             .map((id) => (
               <Note noteData={noteData[id]} id={id} onChange={onSelected} key={noteData, id} isChecked={selectedNotes.indexOf(id) >= 0}/>
-            ))} */}
+            ))} *
           {notesTable}
         </TableBody>
       </Table>
@@ -263,25 +369,61 @@ export default function Orders(props) {
         <Link color="primary" href="#" onClick={preventDefault}>
           See more orders
         </Link>
-      </div>
+      </div> */}
     </React.Fragment>
   );
-}
+})
+
+
+// function Note(props){
+//   const noteData = props.noteData;
+//   const id = props.id;
+//   const isChecked = props.isChecked;
+
+//   // const [update, setUpdate] = React.useState(true)
+
+//   const [selectedNotes, setSelectedNotes] = useSelected(function(){})
+//   console.log(selectedNotes)
+
+//   // console.log("NOTE UPDATING!!")
+
+//   // () => selectedNotes.indexOf(id) >= 0
+//   return (
+//     <TableRow>
+//       <TableCell><Checkbox onChange={(event) => {setSelectedNotes(id)}}  checked={selectedNotes.includes(id)}/></TableCell>
+//       <TableCell>{noteData.facility}</TableCell>
+//       {/* <TableCell>{noteData[id].approved.toString()}</TableCell> */}
+//       <TableCell>{noteData.approved ? "✔️" : "❌"}</TableCell>
+//       <TableCell>{noteData.sent ? "✔️" : "❌"}</TableCell>
+//       <TableCell>{noteData.sender}</TableCell>
+//       <TableCell align="right">{noteData.note}</TableCell>
+//     </TableRow>
+//   );
+// }
 
 
 function Note(props){
   const noteData = props.noteData;
   const id = props.id;
-  const isChecked = props.isChecked;
-
+  const [something, setSomething] = React.useState(0);
+  var isChecked = props.isChecked;
+  // const [isChecked, setIsChecked] = React.useState(props.isChecked);
   const [update, setUpdate] = React.useState(true)
 
-  // console.log("NOTE UPDATING!!")
+  console.log("NOTE UPDATING:", isChecked)
+
+  function onChangeCallback(event){
+    props.onChange(event, id, !(isChecked.includes(id))); 
+    setUpdate(!update);
+    setSomething(something + 1);
+    console.log("Check box on change was called with", something+1)
+    console.log("Check box ", id, isChecked.includes(id))
+  }
 
   // () => selectedNotes.indexOf(id) >= 0
   return (
     <TableRow>
-      <TableCell><Checkbox onChange={(event) => {props.onChange(event, id, !(isChecked.includes(id))); setUpdate(!update)}}  checked={isChecked.includes(id)}/></TableCell>
+      <TableCell><Checkbox onChange={onChangeCallback}  checked={isChecked.includes(id)}/></TableCell>
       <TableCell>{noteData.facility}</TableCell>
       {/* <TableCell>{noteData[id].approved.toString()}</TableCell> */}
       <TableCell>{noteData.approved ? "✔️" : "❌"}</TableCell>
